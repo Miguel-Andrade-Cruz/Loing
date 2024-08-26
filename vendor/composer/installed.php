e16875185a9e68fb92571740949692bdb7374b11
Angelium-Eddra <angelium.eddra1972@gmail.com>
2024-08-25 22:16:41 -0300

    adding again the autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'minuz/loing',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6f8f7195fb312de09cf6e9e243b10b7ffbdc8028',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.1',
+            'version' => '6.10.1.0',
+            'reference' => '500501c2ce893c824c801da135d02661199f60c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'minuz/loing' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6f8f7195fb312de09cf6e9e243b10b7ffbdc8028',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
