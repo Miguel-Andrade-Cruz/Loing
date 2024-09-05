bb2f3535f9917a9c2538d8b43e13741e24c2a364
Angelium-Eddra <angelium.eddra1972@gmail.com>
2024-09-05 16:29:09 -0300

    almost all done with docker compose

--- app/Public/index.php
+++ app/Public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once '../vendor/autoload.php';
-require_once '../src/Routes/Routes.php';
+require_once '../../vendor/autoload.php';
+require_once '../Src/Routes/Routes.php';
 require_once '../Src/Config/bootstrap.php';
 
 use Minuz\Api\Core\Core;
